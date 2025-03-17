f36a13e53a020f56d9c6ed7ac0abeef31848a524
moreiralud <ludmila.brmr@gmail.com>
2025-03-17 10:59:09 -0300

    Initial commit

---
+++ src/main/java/com/gastrogo/gastrogo/GastroGoApplication.java
@@ -0,0 +1,13 @@
+package com.gastrogo.gastrogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastroGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastroGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gastrogo/gastrogo/GastroGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastrogo.gastrogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastroGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
